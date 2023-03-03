5879611f41321b2e69f00b9bd23e8d5cb5e8a987
“yeyoung8805” <yyk8805@naver.com>
2023-03-03 19:00:33 +0900

    [dataType] 기본 데이터 타입

---
+++ dataType.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+//Int
+var someInt: Int = -100
+someInt = 100
+
+//UInt (Unsigned Int =부호가 없는 Int)
+var someUInt: UInt = 200
+
+var someFloat: Float = 1.1
+someFloat = 1
+print(someFloat)
+
+var someDouble: Double = 1.1
+someDouble = 1
+
+var someBool: Bool = true
+someBool= false
+
+var someCharacter: Character = "가"
+someCharacter = "A"
+
+var someString: String = "안녕하세요"
+
+// 타입 추론
+var number = 10
+
