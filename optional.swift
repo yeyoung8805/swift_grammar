df247b397d688138f3374b3b7dbe2ff041695991
“yeyoung8805” <yyk8805@naver.com>
2023-03-04 17:10:44 +0900

    [옵셔널] ?(=optional)  사용법

---
+++ optional.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var name: String? //nil
+
+var optionalName: String? = "Young" // ?(=optional) 에도 일반값 "Young"을 넣을 수 있다.
+print(optionalName) //Optional("Young")
+
+// var requiredName: String = optionalName //(에러) 잘못된 문법! optional이 아닌 String에 optional 인 String? 을 대입할 수 없다!
+
+
